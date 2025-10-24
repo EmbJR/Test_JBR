5e091840291c353339efd4b68aec8ca330eb3657
Jaydeep_Desktop <embeddeddesk@gmail.com>
2025-10-24 23:56:49 +0530

    Added text file.

--- Python/Array/Array1.py
+++ Python/Array/Array1.py
@@ -1,6 +1,10 @@
 import numpy as np
-
 arra = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print("Array shape:", arra.shape)
 print("Array size:", arra.size)
 print("Array dimension:", arra.ndim)
+
+file = open('TFile.txt', 'r')
+for line in file:
+    print(line)
+file.close()
