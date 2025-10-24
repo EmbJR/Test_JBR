3613d4c1e6693b7ae26d5100a1b7a8b33bcf09d1
Jaydeep_Desktop <embeddeddesk@gmail.com>
2025-10-24 23:33:08 +0530

    minor changes

--- Python/Array/Array1.py
+++ Python/Array/Array1.py
@@ -1,3 +1,6 @@
 import numpy as np
 
-arra = np.array()
+arra = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print("Array shape:", arra.shape)
+print("Array size:", arra.size)
+print("Array dimension:", arra.ndim)
